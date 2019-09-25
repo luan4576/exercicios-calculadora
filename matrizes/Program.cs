b031ffacd7dbe48b4c2666bbac8c80e3c92b540e
luan4576 <luansouza2002.ls@gmail.com>
2019-09-25 11:28:29 -0300

    novos ex

---
+++ matrizes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite 6 numeros");
+            int a =( Console.ReadLine());
+
+            
+            int[] vetor = new int (6);
+            for (int i =0; i <6; i++);
+
+        
+
+
+
+
+        }
+    }
+}
---
+++ triangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("alt do trian");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("qt de tr");
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i=0;i<c;i++){
+                string estrelas ="";
+
+                for (int j=0;j<=a;j++) {
+                   estrelas +="*";
+                        Console.WriteLine(estrelas);
+                    }
+                  
+                }                
+            }
+
+        }
+    }
+
